c12e4557c849204c508395801f0026e2d08b2374
Naseer.K <naseerkovvuru2003@gmail.com>
2025-08-19 14:08:45 +0530

    removed somethings

--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -153,11 +153,7 @@ const Works = () => {
             </div>
             {/* mobile preview image */}
             <div className="relative flex items-center justify-center px-10 md:hidden h-[400px]">
-              <img
-                src={project.bgImage}
-                alt={`${project.name}-bg-image`}
-                className="object-cover w-full h-full rounded-md brightness-50"
-              />
+              <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
               <img
                 src={project.image}
                 alt={`${project.name}-image`}

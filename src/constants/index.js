4abb1de263341b106280cb48aba4277752971f61
Naseer.K <naseerkovvuru2003@gmail.com>
2025-08-19 14:03:03 +0530

    photo pending

---
+++ src/constants/index.js
@@ -0,0 +1,175 @@
+// index.js
+export const servicesData = [
+  {
+    title: "FullStack Development",
+    description:
+      "Your business deserves a fast, secure, and future-proof digital foundation. I develop custom web apps with clean architecture, optimized databases, and seamless integrations—ensuring reliability at every layer.",
+    items: [
+      {
+        title: "Backend Engineering",
+        description: "(REST/GraphQL APIs, Secure Auth Systems)",
+      },
+      {
+        title: "Frontend Excellence",
+        description: "(React, Vue, TypeScript, Interactive UI/UX)",
+      },
+      {
+        title: "Database Design",
+        description: "(SQL/NoSQL Optimization, Scalable Structures)",
+      },
+    ],
+  },
+  {
+    title: "Frontend Libraries & Frameworks",
+    description:
+      "Building interactive and responsive user interfaces is essential in today’s digital world, ensuring smooth user experiences across devices. Modern frontend tools make it easier to create scalable, dynamic, and visually appealing web applications.",
+    items: [
+      {
+        title: "React.js",
+        description: "Build dynamic, reusable, and efficient user interfaces",
+      },
+      {
+        title: "Next.js",
+        description: "Fast, scalable, and SEO-friendly web applications.",
+      },
+      {
+        title: "Express.js",
+        description: "Lightweight, flexible, and fast backend framework.",
+      },
+    ],
+  },
+  {
+    title: "Ui Component Libraries and Styling",
+    description:
+      "UI component libraries provide prebuilt, customizable elements that speed up development and maintain design consistency. They help create modern, responsive, and user-friendly interfaces efficiently.",
+    items: [
+      {
+        title: "Tailwind CSS",
+        description: "Utility-first styling for responsive, modern designs",
+      },
+      {
+        title: "GSAP",
+        description: "Smooth, high-performance animations for modern websites.",
+      },
+      {
+        title: "ShadCN UI",
+        description: "Accessible, customizable, and modern UI components.",
+      },
+    ],
+  },
+  {
+    title: "Programming Languages & Databases",
+    description:
+      "Programming languages are the foundation of software development, enabling us to communicate with computers and build applications. They power everything from simple scripts to complex systems driving today’s digital world.",
+    items: [
+      {
+        title: "JavaScript, Typescript",
+        description: "Dynamic language powering interactive, modern web",
+      },
+      {
+        title: "Java",
+        description: "Robust, versatile language for cross-platform development",
+      },
+      {
+        title: "SQL, MongoDB",
+        description: "Structured queries for managing relational databases,Flexible NoSQL database for scalable applications.",
+      },
+    ],
+  },
+];
+export const projects = [
+  {
+    id: 1,
+    name: "CodeSync - An Interview Platform",
+    description:
+      "A smart interview platform that connects candidates and recruiters with real-time coding, video, and evaluation tools—streamlining the entire hiring process. Built for efficiency, fairness, and seamless experiences.",
+
+    href: "https://interview-platform-naseer.vercel.app/",
+    image: "/assets/projects/interview-plat.png",
+    bgImage: "/assets/backgrounds/blanket.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Next.js" },
+      { id: 3, name: "Node.js" },
+      { id: 4, name: "Convex DB" },
+      { id: 5, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Socially - Social Media Platform",
+    description:
+      "A modern social media platform where users can connect, share, and engage with content in real time. Built with a sleek UI and smooth authentication for a seamless social experience.",
+    href: "https://socially-roan.vercel.app/",
+    image: "/assets/projects/socially.jpg",
+    bgImage: "/assets/backgrounds/curtains.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Next.js" },
+      { id: 3, name: "Postgress SQL" },
+      { id: 4, name: "Tailwind CSS" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Spylt Clone",
+    description:
+      "A feature-rich clone of Spylt that replicates its sleek UI and interactive animations. Designed to showcase modern frontend skills with GSAP, smooth transitions, and responsive layouts",
+    href: "https://spylt-clone-theta.vercel.app/",
+    image: "/assets/projects/spylt.jpg",
+    bgImage: "/assets/backgrounds/map.jpg",
+    frameworks: [
+      { id: 1, name: "React.js" },
+      { id: 2, name: "GSAP" },
+      { id: 3, name: "TailwindCSS" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Velvet Pour - Mojito Website",
+    description:
+      "A visually refreshing Mojito-themed website built with GSAP animations, delivering smooth scroll effects and interactive transitions. Designed to showcase creative frontend motion design with a modern aesthetic.",
+    href: "https://velvet-pour-five-mu.vercel.app/",
+    image: "/assets/projects/velvetPour.jpg",
+    bgImage: "/assets/backgrounds/poster.jpg",
+    frameworks: [
+      { id: 1, name: "React.js " },
+      { id: 2, name: "Tailwind CSS" },
+      { id: 3, name: "GSAP" },
+    ],
+  },
+  {
+    id: 5,
+    name: "BookShelf - Book store App",
+    description:
+      "A book store app where you can purchase books and a special admin section to track the records of books and users.",
+    href: "",
+    image: "/assets/projects/books-store.jpg",
+    bgImage: "/assets/backgrounds/table.jpg",
+    frameworks: [
+      { id: 1, name: "React" },
+      { id: 2, name: "Express" },
+      { id: 3, name: "MongoDB" },
+      { id: 4, name: "Node.js" },
+    ],
+  },
+  {
+    id: 6,
+    name: "ThinkPad - just a notes app",
+    description:
+      "A simple notes app where you can create, read, update, and delete notes",
+    href: "",
+    image: "/assets/projects/thinkPad.jpg",
+    bgImage: "/assets/backgrounds/curtains.jpg",
+    frameworks: [
+      { id: 1, name: "React.js" },
+      { id: 2, name: "Node.js" },
+      { id: 3, name: "MongoDB" },
+      { id: 4, name: "Daisy UI" },
+    ],
+  },
+];
+export const socials = [
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/kovvuru-naseer-b5b98725a/" },
+  { name: "GitHub", href: "https://github.com/Naseer2003" },
+];
---
+++ src/sections/About.jsx
@@ -0,0 +1,65 @@
+import { useRef } from "react";
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { AnimatedTextLines } from "../components/AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const About = () => {
+  const text = `Passionate about clean architecture
+    I build scalable, high-performance solutions
+    from prototype to production`;
+  const aboutText = `Obsessed with crafting smooth, engaging web apps—from animation-rich React/Next.js interfaces to scalable backends with MongoDB & PostgreSQL. Every project I build blends performance, design, and user delight.
+
+When I’m not coding:
+⚡️ Experimenting with GSAP and cool UI effects
+📚 Sharing projects & ideas with the dev community
+📖 Reading books to spark new ideas
+🎶 Vibing to music while pushing commits`;
+
+  const imgRef = useRef(null);
+  useGSAP(() => {
+    gsap.to("#about", {
+      scale: 0.95,
+      scrollTrigger: {
+        trigger: "#about",
+        start: "bottom 80%",
+        end: "bottom 20%",
+        scrub: true,
+        markers: false,
+      },
+      ease: "power1.inOut",
+    });
+
+    gsap.set(imgRef.current, {
+      clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
+    });
+    gsap.to(imgRef.current, {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      duration: 2,
+      ease: "power4.out",
+      scrollTrigger: { trigger: imgRef.current },
+    });
+  });
+  return (
+    <section id="about" className="min-h-screen bg-black rounded-b-4xl">
+      <AnimatedHeaderSection
+        subTitle={"Code with purpose, Built to scale"}
+        title={"About"}
+        text={text}
+        textColor={"text-white"}
+        withScrollTrigger={true}
+      />
+      <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
+        <img
+          ref={imgRef}
+          src="images/man.jpg"
+          alt="man"
+          className="w-md rounded-3xl"
+        />
+        <AnimatedTextLines text={aboutText} className={"w-full"} />
+      </div>
+    </section>
+  );
+};
+
+export default About;

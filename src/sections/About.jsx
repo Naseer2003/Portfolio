758796f3653e43159e6612333d4f014dd238ea15
Naseer.K <naseerkovvuru2003@gmail.com>
2025-08-19 15:31:44 +0530

    all done

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -52,7 +52,7 @@ When I’m not coding:
       <div className="flex flex-col items-center justify-between gap-16 px-10 pb-16 text-xl font-light tracking-wide lg:flex-row md:text-2xl lg:text-3xl text-white/60">
         <img
           ref={imgRef}
-          src="images/man.jpg"
+          src="images/profile-pic.jpg"
           alt="man"
           className="w-md rounded-3xl"
         />
